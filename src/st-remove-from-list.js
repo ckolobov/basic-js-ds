f90b6bafd706eb2493c6ba10b0e1ca6d51b08a10
Constantine Kolobov <kolobovconstantine@gmail.com>
2021-10-17 19:12:24 +0300

    feat: solve st-stack and st-remove-from-list

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+  let first = l;
+
+  while (first.value === k) {
+    first = first.next;
+  }
+
+  let current = first;
+  let next = current.next;
+
+  while (next) {
+    if (next.value !== k) {
+      current.next = next;
+      current = next;
+    }
+    next = next.next;
+  }
+
+  return first;
 }
